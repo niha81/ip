917a4a62ca127ae4f22d16b7296ee01dc4da907a
niha81 <nihadolly@gmail.com>
2025-01-23 17:53:27 +0800

    level 1 done

--- src/main/java/Woogie.java
+++ src/main/java/Woogie.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 public class Woogie {
     public static void main(String[] args) {
         String line = "\n----------------------------------------\n";
-        String printContent = line + "Hello! I'm Woogie\n" +
-                "What can I do for you?" + line + "Bye. Hope to see you again soon!" + line;
-        System.out.println(printContent);
+        String greeting = line + "Hello! I'm Woogie\n" +
+                "What can I do for you?" + line;
 
+        System.out.println(greeting);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(line + "Bye. Hope to see you again soon!" + line);
+                break;
+            }
+
+            System.out.println(line + input + line);
+        }
+
+        scanner.close();
     }
 }
