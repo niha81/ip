ae93e79d25726a4bd94bde9c2c0c047faa4bb82c
niha81 <nihadolly@gmail.com>
2025-01-23 18:32:36 +0800

    add, list

--- src/main/java/Woogie.java
+++ src/main/java/Woogie.java
@@ -8,6 +8,9 @@ public class Woogie {
 
         System.out.println(greeting);
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
@@ -18,7 +21,25 @@ public class Woogie {
                 break;
             }
 
-            System.out.println(line + input + line);
+            if (input.equalsIgnoreCase("list")) {
+                if (taskCount == 0) {
+                    System.out.println(line + "nothing here yet TT" + line);
+                } else {
+                    String taskList = "";
+                    for (int i = 0; i < taskCount; i++) {
+                        taskList = taskList + (i+1) + ". " + tasks[i] + "\n";
+                    }
+                    System.out.println(line + taskList + line);
+                }
+            } else {
+                if (taskCount < 100) {
+                    tasks[taskCount] = input;
+                    taskCount++;
+                    System.out.println(line + "added: " + input + line);
+                } else {
+                    System.out.println(line + "no more space <3" + line);
+                }
+            }
         }
 
         scanner.close();
