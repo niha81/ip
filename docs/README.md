4245a0d516d2c6b58de969262a973be1f3e56514
niha81 <nihadolly@gmail.com>
2025-01-23 17:23:12 +0800

    level 0 done

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Woogie User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Woogie.java
@@ -0,0 +1,9 @@
+public class Woogie {
+    public static void main(String[] args) {
+        String line = "\n----------------------------------------\n";
+        String printContent = line + "Hello! I'm Woogie\n" +
+                "What can I do for you?" + line + "Bye. Hope to see you again soon!" + line;
+        System.out.println(printContent);
+
+    }
+}
