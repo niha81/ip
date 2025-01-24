9e63b63b99009474c8ea4f0914a0cd7f428afa71
niha81 <nihadolly@gmail.com>
2025-01-24 10:52:30 +0800

    handle errors

--- src/main/java/Woogie.java
+++ src/main/java/Woogie.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Woogie {
     public static void main(String[] args) {
-        String line = "\n----------------------------------------\n";
+        String line = "\n------------------------------------------------\n";
         String greeting = line + "Hello! I'm Woogie\n" + "What can I do for you?" + line;
         System.out.println(greeting);
 
@@ -63,6 +63,10 @@ public class Woogie {
                 if (taskCount >= 100) {
                     System.out.println(line + "no more space <3" + line);
                 }
+                if (input.length() <= 5 || input.substring(5).trim().isEmpty()) {
+                    System.out.println(line + "todo's description cannot be empty, pls add one >:(" + line);
+                    continue;
+                }
                 String description = input.substring(5);
                 tasks[taskCount] = new ToDo(description);
                 taskCount++;
@@ -72,7 +76,22 @@ public class Woogie {
                 if (taskCount >= 100) {
                     System.out.println(line + "no more space <3" + line);
                 }
-                String[] parts = input.split(" /by ");
+
+                if (!input.contains(" /by ")) {
+                    System.out.println(line + "you can't have a deadline without a deadline, add a /by :)" + line);
+                    continue;
+                }
+
+                String[] parts = input.split(" /by ", 2);
+                if (parts[0].length() <= 9 || parts[0].substring(9).trim().isEmpty()) {
+                    System.out.println(line + "deadline's description cannot be empty, pls add one >:(" + line);
+                    continue;
+                }
+                if (parts[1].trim().isEmpty()) {
+                    System.out.println(line + "deadline's /by time cannot be empty!" + line);
+                    continue;
+                }
+
                 String description = parts[0].substring(9);
                 String by = parts[1];
                 tasks[taskCount] = new Deadline(description, by);
@@ -83,7 +102,30 @@ public class Woogie {
                 if (taskCount >= 100) {
                     System.out.println(line + "no more space <3" + line);
                 }
-                String[] parts = input.split(" /from | /to");
+
+                if (!input.contains(" /from ")) {
+                    System.out.println(line + "i need to know when you event starts, pls add a /from :<" + line);
+                    continue;
+                }
+                if (!input.contains(" /to ")) {
+                    System.out.println(line + "i need to know when you event ends, pls add a /to :<" + line);
+                    continue;
+                }
+
+                String[] parts = input.split(" /from | /to ", 3);
+                if (parts[0].length() <= 6 || parts[0].substring(6).trim().isEmpty()) {
+                    System.out.println(line + "event's description cannot be empty, pls add one >:(" + line);
+                    continue;
+                }
+                if (parts[1].trim().isEmpty()) {
+                    System.out.println(line + "event's /from time cannot be empty!" + line);
+                    continue;
+                }
+                if (parts[2].trim().isEmpty()) {
+                    System.out.println(line + "event's /to time cannot be empty!" + line);
+                    continue;
+                }
+
                 String description = parts[0].substring(6);
                 String from = parts[1];
                 String to = parts[2];
@@ -92,10 +134,11 @@ public class Woogie {
                 System.out.println(line + "Got it. I've added this task:\n  " + tasks[taskCount - 1] + "\nNow you have "
                         + taskCount + " tasks in the list." + line);
             } else {
-                System.out.println(line + "idk this command ;-;" + line);
+                System.out.println(line + "sorry idk this command ;-;" + line);
             }
         }
 
         scanner.close();
     }
+
 }
